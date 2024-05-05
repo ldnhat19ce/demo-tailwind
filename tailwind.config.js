8b7ae3b7a56da7173a954be91d5fdad2b043ca3d
nhatld <nhatld.dev@gmail.com>
2024-05-05 14:39:05 +0700

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,ts}"],
+    content: [
+        "./src/**/*.{html,ts}"
+    ],
     // prefix: 'demo-tw-',
     theme: {
         fontFamily: {
             // sans: ["Graphik", "sans-serif"],
             sans: ["Segoe UI Historic", "Segoe UI", "Helvetica", "Arial", "sans-serif"]
         },
-        extend: {},
+        colors: {
+            'lemon-whisper': '#FFB30E'
+        },
+        textColor: {
+            'grey-900': '#212121',
+            'lemon-whisper': '#FFB30E',
+            'black-panther': '#424242'
+        },
+        width: {
+            '1-5em': '1.5em'
+        },
+        height: {
+            '1-5em': '1.5em'
+        },
+        backgroundSize: {
+            '100': '100%'
+        },
+        backgroundColor: {
+            '100': '#FCFCFC',
+            'transparent': 'transparent',
+            'wash-me': '#F9FAFD'
+        },
+        borderColor: {
+            'transparent': 'transparent'
+        },
+        flex: {
+
+        },
+        extend: {
+
+        },
     },
     plugins: [
         require("@tailwindcss/typography"),
