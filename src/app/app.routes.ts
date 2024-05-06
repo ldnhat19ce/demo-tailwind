564bdbbf84ec7d4c6aac659c9ece203242242606
nhatld <nhatld.dev@gmail.com>
2024-05-06 15:07:51 +0700

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './common/pages/application/home/home.component';
+import { NotFoundComponent } from './common/pages/general/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '**', component: NotFoundComponent }
+];
